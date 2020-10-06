075f3dfef6383800ee5720bd9c3e749341c45494
Justen Stall <justenstall@users.noreply.github.com>
2020-10-06 15:28:14 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    loadChildren: () => import('./layout/main-layout/main-layout.module').then(m => m.MainLayoutModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
